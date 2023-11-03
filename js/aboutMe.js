4cae20b9ff859ded6a60d9e3db28d19e00790c21
Surafel Araya <surafelay@gmail.com>
2023-11-03 20:46:46 +0300

    Add aboutMe

---
+++ js/aboutMe.js
@@ -0,0 +1,17 @@
+const name = 'Surafel Araya';
+
+const jobTitle = 'Software Developer';
+
+const contactAddresses = [
+  { name: 'Email', address: 'surafelay@gmail.com' },
+  { name: 'Phone Number', address: '+251967532329' },
+];
+
+const onlinePresence = [
+  { name: 'Twitter', url: 'https://twitter.com/surafelaraya' },
+  { name: 'LinkedIn', url: 'https://linkedin/in/surafel-araya' },
+  { name: 'Github', url: 'https://github.io/suraffy' },
+];
+
+const aboutMe =
+  'Software developer and web designer. Passionate about making living easier through programming.';
