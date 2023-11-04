585daa0427bb37438f3c74fbca7a381c0ee1feba
Surafel Araya <surafelay@gmail.com>
2023-11-04 23:21:54 +0300

    Remove EventListener when DOMContentLoaded

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@ import ProjectUI from './projectUI.js';
 const filterList = document.querySelector('.filter-list');
 const paginationList = document.querySelector('.pagination');
 
-document.addEventListener('DOMContentLoaded', skillUI.displaySkills);
-document.addEventListener('DOMContentLoaded', ProjectUI.displayProjects);
+// document.addEventListener('DOMContentLoaded', skillUI.displaySkills);
+// document.addEventListener('DOMContentLoaded', ProjectUI.displayProjects);
+
+skillUI.displaySkills();
+ProjectUI.displayProjects();
 
 paginationList.addEventListener('click', ProjectUI.navigatePagination);
 filterList.addEventListener('click', ProjectUI.filterProjects);
