d9fef50afb87863b1a604708c4a48d571990a907
Surafel Araya <surafelay@gmail.com>
2023-11-12 20:38:22 +0300

    Add skills and projects List

---
+++ src/services/skillsList.js
@@ -0,0 +1,40 @@
+const skillsList = [
+  { name: "HTML5", img: "html5 logo.png", className: "long-width" },
+  {
+    name: "CSS3",
+    img: "img_css.jpg",
+    details: ["Flexbox, CSS Grid", "Media Queries", "CSS Animation"],
+  },
+  {
+    name: "Bootstrap_Taiwild",
+    img: "bootstrap.png",
+    img2: "tailwind css.png",
+  },
+  {
+    name: "JavaScript",
+    img: "JavaScript logo official.png",
+    details: ["ES6+", "OOP, Async JavaScript", "DOM Manipulation"],
+  },
+  {
+    name: "Node.js",
+    img: "320px-Node.js_logo.svg.png",
+    details: ["Express", "REST API", "Security"],
+  },
+  {
+    name: "Vue.js",
+    img: "vue-logo.png",
+    details: ["Vue Router", "Vuex"],
+  },
+  {
+    name: "Mongodb",
+    img: "mongodb-logo.png",
+    className: "long-width",
+  },
+  {
+    name: "Adobe Xd",
+    img: "Adobe-Xd.png",
+    details: ["Web Page Design", "Designing SVG Shapes"],
+  },
+];
+
+export default skillsList;
