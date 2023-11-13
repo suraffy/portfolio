8d1ce2f9130e7f6ee63fe4bd72389da137984e2a
Surafel Araya <surafelay@gmail.com>
2023-11-13 16:42:49 +0300

    Populate projects list in Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,14 @@ import projectsList from "../services/projectsList";
 const Projects = () => {
   console.log(projectsList);
 
+  const filterItems = [
+    { keywork: "all", label: "All" },
+    { keywork: "front-end", label: "Front-End" },
+    { keywork: "full-stack", label: "Back-End" },
+    { keywork: "javascript", label: "Plain JavaScript" },
+    { keywork: "css-animation", label: "CSS-Animation" },
+  ];
+
   return (
     <section id="projects">
       <div className="container">
@@ -13,21 +21,15 @@ const Projects = () => {
             <div className="filter-project flex-row">
               <h4>Filter</h4>
               <ul className="filter-list flex-row">
-                <li className="filter-item" data-filter-item="All">
-                  All
-                </li>
-                <li className="filter-item" data-filter-item="Full-Stack">
-                  Full-Stack
-                </li>
-                <li className="filter-item" data-filter-item="Front-End">
-                  Front-End
-                </li>
-                <li className="filter-item" data-filter-item="Plain-JavaScript">
-                  Plain JavaScript
-                </li>
-                <li className="filter-item" data-filter-item="CSS-Animation">
-                  CSS Animation
-                </li>
+                {filterItems.map((f) => (
+                  <li
+                    key={f.keywork}
+                    className="filter-item"
+                    data-filter-item={f.keywork}
+                  >
+                    {f.label}
+                  </li>
+                ))}
               </ul>
             </div>
 
@@ -36,7 +38,28 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="projects-container flex-column"></div>
+          <div className="projects-container flex-column">
+            {projectsList.map((project) => (
+              <div key={project.title} className="project-box flex-row">
+                <div className="project-profile-img row">
+                  <img src={"/img/web-tech.jpg"} alt="" />
+                </div>
+                <div className="project-details flex-column">
+                  <h2 className="project-title">{project.title}</h2>
+                  {/* <span className="project-type">{project.type}</span> */}
+                  <p className="project-description">{project.desc}</p>
+                  {/* <div className="project-links flex-row">
+                    <a href="" target="_blank" className="btn">
+                      Live Preview
+                    </a>
+                    <a href="" target="_blank">
+                      Source Code
+                    </a>
+                  </div> */}
+                </div>
+              </div>
+            ))}
+          </div>
 
           <div className="project-footer">
             <ul className="pagination flex-row"></ul>
