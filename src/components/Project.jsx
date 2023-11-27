2e9b7e0d6262375d2235122a1cc599ff8b744dfb
Surafel Araya <surafelay@gmail.com>
2023-11-27 19:07:31 +0300

    Separet Project into a module

---
+++ src/components/Project.jsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
+const Project = ({ project }) => {
+  return (
+    <div key={project.title} className="project-box flex-column">
+      <div className="project-profile-img row">
+        <img src={"/img/web-tech.jpg"} alt="" />
+      </div>
+      <div className="project-details flex-column">
+        <h2 className="project-title">{project.title}</h2>
+        <div
+          className="flex-row"
+          style={{
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <span className="tech-used">{project.techUsed}</span>
+          <span className="project-links">
+            <a href="#">
+              Live <FontAwesomeIcon icon={faUpRightFromSquare} />
+            </a>
+            &nbsp;
+            <a href="#">
+              <FontAwesomeIcon
+                className="source-code"
+                icon={faGithub}
+                size="lg"
+              />
+            </a>
+          </span>
+        </div>
+        <p className="project-description">{project.desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
+import Project from "./Project";
 import Filter from "./common/Filter";
 
 import projectsList from "../services/projectsList";
@@ -44,37 +42,7 @@ const Projects = () => {
 
           <div className="projects-container">
             {projects.map((project) => (
-              <div key={project.title} className="project-box flex-column">
-                <div className="project-profile-img row">
-                  <img src={"/img/web-tech.jpg"} alt="" />
-                </div>
-                <div className="project-details flex-column">
-                  <h2 className="project-title">{project.title}</h2>
-                  <div
-                    className="flex-row"
-                    style={{
-                      width: "100%",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <span className="tech-used">{project.techUsed}</span>
-                    <span className="project-links">
-                      <a href="#">
-                        Live <FontAwesomeIcon icon={faUpRightFromSquare} />
-                      </a>
-                      &nbsp;
-                      <a href="#">
-                        <FontAwesomeIcon
-                          className="source-code"
-                          icon={faGithub}
-                          size="lg"
-                        />
-                      </a>
-                    </span>
-                  </div>
-                  <p className="project-description">{project.desc}</p>
-                </div>
-              </div>
+              <Project key={project.title} project={project} />
             ))}
           </div>
 
