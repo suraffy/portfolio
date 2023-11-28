b53eba6165fb298d03f59909b36048a162540241
Surafel Araya <surafelay@gmail.com>
2023-11-28 07:47:55 +0300

    Implement Form Handling

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -1,6 +1,13 @@
+import { useForm } from "@formspree/react";
+
 const Form = () => {
+  const [state, handleSubmit] = useForm("mrgwwkwv");
+  if (state.succeeded) {
+    return <p>Thanks for Sending!</p>;
+  }
+
   return (
-    <form action="" className="flex-column">
+    <form onSubmit={handleSubmit} className="flex-column">
       <input type="text" name="name" placeholder="Your Name" />
       <input type="email" name="email" placeholder="Your Email" required />
       <input type="text" name="subject" placeholder="Subject" />
